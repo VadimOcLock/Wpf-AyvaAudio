d9d4d0b1c135c9077c06ab23b9d73a5c054de60a
Vadim Kozenkov <vadimoclock@gmail.com>
2022-05-24 17:25:07 +0300

    Создание EditViewModel

---
+++ WPFNaudio/MVVM/ViewModels/EditViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFNaudio.MVVM.ViewModels.Base;
+
+namespace WPFNaudio.MVVM.ViewModels
+{
+    internal class EditViewModel : ViewModel
+    {
+        public EditViewModel()
+        {
+
+        }
+    }
+}
