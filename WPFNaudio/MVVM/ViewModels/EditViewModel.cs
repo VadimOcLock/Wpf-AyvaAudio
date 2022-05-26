629997c1afa14f3e879388e072d1543548e672bb
Vadim Kozenkov <vadimoclock@gmail.com>
2022-05-26 12:32:52 +0300

    Первичная валидация ввода громкости аудиофайла

--- WPFNaudio/MVVM/ViewModels/EditViewModel.cs
+++ WPFNaudio/MVVM/ViewModels/EditViewModel.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.ComponentModel;
 using WPFNaudio.MVVM.ViewModels.Base;
 
 namespace WPFNaudio.MVVM.ViewModels
 {
-    public class EditViewModel : ViewModel
+    public class EditViewModel : ViewModel, IDataErrorInfo
     {
         private double _volumeValue = 1;
 
@@ -15,10 +16,42 @@ namespace WPFNaudio.MVVM.ViewModels
             }
             set
             {
-                Set(ref _volumeValue, value);
+                if (value < 0)
+                {
+                    Set(ref _volumeValue, 0);
+                }
+                else if (value > 200)
+                {
+                    Set(ref _volumeValue, 200);
+                }
+                else
+                {
+                    Set(ref _volumeValue, value);
+                }
             }
         }
 
+        public string Error { get { return null; } }
+
+        public string this[string name] 
+        {
+            get 
+            {
+                string result = null;
+
+                switch (name)
+                {
+                    case "VolumeValue":
+                        if (VolumeValue == null)
+                            result = "Поле не может быть пустым";
+                        else if (VolumeValue < 0 || VolumeValue > 200)
+                            result = "Введите значение от 0 до 200";
+                        break;
+                }
+
+                return result;
+            }
+        }
 
         public EditViewModel()
         {
