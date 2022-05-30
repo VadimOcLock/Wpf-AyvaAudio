31e41c792b6d8e67ddf06af2277647ae8ce36ce1
Vadim Kozenkov <vadimoclock@gmail.com>
2022-05-30 10:42:40 +0300

    Биндинги комманд на кнопки, создание MergeMp3View и MergeMp3ViewModel

--- WPFNaudio/MVVM/ViewModels/MergerViewModel.cs
+++ WPFNaudio/MVVM/ViewModels/MergerViewModel.cs
@@ -1,10 +1,31 @@
-﻿using WPFNaudio.MVVM.ViewModels.Base;
+﻿using WPFNaudio.Core;
+using WPFNaudio.MVVM.View.Windows;
+using WPFNaudio.MVVM.ViewModels.Base;
 
 namespace WPFNaudio.MVVM.ViewModels
 {
     public class MergerViewModel : ViewModel
     {
+        public LambdaCommand MergeMp3Command { get; }
+        public LambdaCommand CombineMp3Command { get; }
+
         public MergerViewModel()
+        {
+            MergeMp3Command = new LambdaCommand(OnMergeMp3CommandExecuted, CanMergeMp3CommandExecute);
+            CombineMp3Command = new LambdaCommand(OnCombineMp3CommandExecuted, CanCombineMp3CommandExecute);
+        }
+
+        private bool CanMergeMp3CommandExecute(object p) => true;
+        private void OnMergeMp3CommandExecuted(object p)
+        {
+            var window = new MergeMp3View();
+            var vm = new MergeMp3ViewModel();
+
+            window.DataContext = vm;
+            if (window.ShowDialog() == true) { }
+        }
+        private bool CanCombineMp3CommandExecute(object p) => true;
+        private void OnCombineMp3CommandExecuted(object p)
         {
 
         }
